b42143898da91b3c5d9263fb3a05526c8398bb36
Fred Douglas <fed2@case.edu>
2021-10-11 13:23:16 -0500

    add printEqualizerAlreadyFreq for when you already have Fourier output

--- equalizer.cc
+++ equalizer.cc
@@ -1,36 +1,44 @@
 #include "equalizer.h"
 
-#include <complex>
 #include <fftw3.h>
-using ComplexDouble = std::complex<double>;
 
-void printEqualizer(const float* samples, int num_frames, int num_channels)
+#include "constants.h"
+
+void printEqualizer(const float* samples, int num_frames)
 {
   ComplexDouble* transformed = new ComplexDouble[num_frames / 2 + 1];
   double orig_real[num_frames];
   for (int i=0; i<num_frames; i++)
-    orig_real[i] = samples[i * num_channels];
+    orig_real[i] = samples[i * kNumChannels];
 
-  fftw_plan fftw_forward = fftw_plan_dft_r2c_1d(num_frames, orig_real, reinterpret_cast<fftw_complex*>(transformed), FFTW_ESTIMATE);
+  fftw_plan fftw_forward =
+    fftw_plan_dft_r2c_1d(num_frames, orig_real,
+                         reinterpret_cast<fftw_complex*>(transformed),
+                         FFTW_ESTIMATE);
   fftw_execute(fftw_forward);
   fftw_destroy_plan(fftw_forward);
 
+  printEqualizerAlreadyFreq(transformed, num_frames);
+  delete[] transformed;
+}
+
+void printEqualizerAlreadyFreq(ComplexDouble* freq_buckets, int orig_num_frames)
+{
   constexpr int kMaxHeight = 50;
-  const int columns = num_frames / 2 + 1; // including \n char at end
+  const int columns = orig_num_frames / 2 + 1; // including \n char at end
   char* bars = new char[columns * kMaxHeight + 1];
   for (int height = kMaxHeight; height > 0; height--)
   {
     int y = kMaxHeight - height;
-    for (int x = 0; x < num_frames / 2; x++)
-      if (abs(transformed[x].real()) > height)
+    for (int x = 0; x < orig_num_frames / 2; x++)
+      if (abs(freq_buckets[x].real()) > height)
         bars[columns * y + x] = '0';
       else
         bars[columns * y + x] = ' ';
-    bars[columns * y + num_frames / 2] = '\n';
+    bars[columns * y + orig_num_frames / 2] = '\n';
   }
   printf("\e[1;1H\e[2J");
   bars[columns * kMaxHeight] = 0;
   printf("%s", bars);
-  delete[] transformed;
   delete[] bars;
 }
--- equalizer.h
+++ equalizer.h
@@ -1,6 +1,13 @@
 #ifndef CLICKITONGUE_EQUALIZER_H_
 #define CLICKITONGUE_EQUALIZER_H_
 
-void printEqualizer(const float* samples, int num_frames, int num_channels);
+#include <complex>
+using ComplexDouble = std::complex<double>;
+
+void printEqualizer(const float* samples, int num_frames);
+
+// length of freq_buckets should be orig_num_frames / 2 + 1
+// (i.e. the output of a real-to-complex FFT with length orig_num_frames input)
+void printEqualizerAlreadyFreq(ComplexDouble* freq_buckets, int orig_num_frames);
 
 #endif // CLICKITONGUE_EQUALIZER_H_
