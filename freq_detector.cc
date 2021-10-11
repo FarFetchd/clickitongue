02f0b40447dd4b06d7939197471b7e5fc2487fc5
Fred Douglas <fed2@case.edu>
2021-10-11 13:27:15 -0500

    add FreqDetector

---
+++ freq_detector.cc
@@ -0,0 +1,112 @@
+#include "freq_detector.h"
+
+#include <fftw3.h>
+
+#include "equalizer.h"
+
+FreqDetector::FreqDetector(BlockingQueue<Action>* action_queue,
+                           float lowpass_percent,
+  float highpass_percent,
+  double low_on_thresh,
+  double low_off_thresh,
+  double high_on_thresh,
+  double high_off_thresh,
+                           std::vector<int>* cur_frame_dest)
+  : Detector(action_queue),
+  action_(Action::RecordCurFrame),
+
+  lowpass_percent_(lowpass_percent),
+  highpass_percent_(highpass_percent),
+  low_on_thresh_(low_on_thresh),
+  low_off_thresh_(low_off_thresh),
+  high_on_thresh_(high_on_thresh),
+  high_off_thresh_(high_off_thresh)
+
+{
+  setCurFrameDest(cur_frame_dest);
+  setCurFrameSource(&cur_frame_);
+}
+
+FreqDetector::FreqDetector(BlockingQueue<Action>* action_queue, Action action)
+  : Detector(action_queue), action_(action)
+{
+//    assert(action_ != Action::RecordCurFrame);
+}
+
+void FreqDetector::processAudio(const Sample* cur_sample, int num_frames)
+{
+  if (!(num_frames == 256 || num_frames == 128 || num_frames == 512 || num_frames == 1024))
+    return;
+
+  double* orig_real = new double[num_frames];
+  for (int i=0; i<num_frames; i++)
+    orig_real[i] = cur_sample[i * kNumChannels];
+  int num_buckets = num_frames / 2 + 1;
+  ComplexDouble* transformed = new ComplexDouble[num_buckets];
+
+  fftw_plan fftw_forward =
+    fftw_plan_dft_r2c_1d(num_frames, orig_real,
+                         reinterpret_cast<fftw_complex*>(transformed),
+                         FFTW_ESTIMATE); // TODO wisdom
+  fftw_execute(fftw_forward);
+  fftw_destroy_plan(fftw_forward);
+
+  static int print_once_per_10ms_chunks = 0;
+  if (++print_once_per_10ms_chunks == 20)
+  {
+    printEqualizerAlreadyFreq(transformed, num_frames);
+    print_once_per_10ms_chunks=0;
+  }
+
+  cur_frame_ += num_frames;
+
+  int last_low_bucket = round(lowpass_percent_ * num_buckets);
+  double avg_low = 0;
+  for (int i = 0; i <= last_low_bucket; i++)
+    avg_low += fabs(transformed[i].real());
+  avg_low /= (double)(last_low_bucket + 1);
+
+  int first_high_bucket = round(highpass_percent_ * num_buckets);
+  double avg_high = 0;
+  int high_above_1 = 0;
+  for (int i = first_high_bucket; i < num_buckets; i++)
+  {
+    double val = fabs(transformed[i].real());
+    avg_high += val;
+    if (val > 1.0)
+      high_above_1++;
+  }
+  avg_high /= (double)(num_buckets - first_high_bucket);
+
+  // TODO make this a configurable param
+  bool many_high_spikes = (float)high_above_1 / (float)(num_buckets - first_high_bucket) > 0.5;
+
+  if (avg_low > low_on_thresh_ && avg_high > high_on_thresh_ && many_high_spikes && !mouse_down_)
+  {
+    if (action_ == Action::RecordCurFrame)
+      kickoffAction(Action::RecordCurFrame);
+    else
+      kickoffAction(Action::LeftDown);
+    mouse_down_ = true;
+  }
+  else if (avg_low < low_off_thresh_ && avg_high < high_off_thresh_ && mouse_down_)
+  {
+    if (action_ != Action::RecordCurFrame)
+      kickoffAction(Action::LeftUp);
+    mouse_down_ = false;
+  }
+  delete[] orig_real;
+  delete[] transformed;
+}
+
+int freqDetectorCallback(const void* inputBuffer, void* outputBuffer,
+                         unsigned long framesPerBuffer,
+                         const PaStreamCallbackTimeInfo* timeInfo,
+                         PaStreamCallbackFlags statusFlags, void* userData)
+{
+  FreqDetector* detector = (FreqDetector*)userData;
+  const Sample* rptr = (const Sample*)inputBuffer;
+  if (inputBuffer != NULL)
+    detector->processAudio(rptr, framesPerBuffer);
+  return paContinue;
+}
---
+++ freq_detector.h
@@ -0,0 +1,60 @@
+#ifndef CLICKITONGUE_FREQ_DETECTOR_H_
+#define CLICKITONGUE_FREQ_DETECTOR_H_
+
+#include "portaudio.h"
+
+#include "actions.h"
+#include "constants.h"
+#include "detector.h"
+
+class FreqDetector : public Detector
+{
+public:
+  // For training. Saves the frame indices of all detected events into
+  // cur_frame_dest, and does nothing else.
+  FreqDetector(BlockingQueue<Action>* action_queue,
+               float lowpass_percent,
+  float highpass_percent,
+  double low_on_thresh,
+  double low_off_thresh,
+  double high_on_thresh,
+  double high_off_thresh,
+               std::vector<int>* cur_frame_dest);
+
+  // Kicks off 'action' at each detected event.
+  FreqDetector(BlockingQueue<Action>* action_queue,
+               Action action);
+
+  void processAudio(const Sample* cur_sample, int num_frames) override;
+
+private:
+  const float some_param_ = 1;
+  const Action action_;
+
+  // the percentile bucket (rounded) of the Fourier output that is the highest
+  // we should consider in the lowpass half.
+  const float lowpass_percent_ = 0.035;
+  // ditto, lowest for the highpass half.
+  const float highpass_percent_ = 0.5;
+
+  // The average value threshold needed to be crossed by the lowpass half in
+  // order to move to "mouse down" state.
+  const double low_on_thresh_ = 10;
+  // etc
+  const double low_off_thresh_ = 3;
+  const double high_on_thresh_ = 0.75;
+  const double high_off_thresh_ = 0.1;
+
+  // our understanding of the current state of the mouse button
+  bool mouse_down_ = false;
+
+  // only needs to be kept up to date if you plan to use RecordCurFrame
+  int cur_frame_ = 0;
+};
+
+int freqDetectorCallback(const void* inputBuffer, void* outputBuffer,
+                         unsigned long framesPerBuffer,
+                         const PaStreamCallbackTimeInfo* timeInfo,
+                         PaStreamCallbackFlags statusFlags, void* userData);
+
+#endif // CLICKITONGUE_FREQ_DETECTOR_H_
